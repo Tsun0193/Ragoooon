5710aaff59f435461ee366a05082c5b7d4d8347c
Tsun0193 <dduy193@gmail.com>
2024-12-02 00:58:46 +0700

    Implemented multiple chunking modules

--- Core/Chunking/SemanticDoubleMergingSplitter.py
+++ Core/Chunking/SemanticDoubleMergingSplitter.py
@@ -1,20 +1,25 @@
-import pandas as pd
-import nltk
-from snowflake.snowpark.types import StringType, StructField, StructType
 from llama_index.core.node_parser import SemanticDoubleMergingSplitterNodeParser, LanguageConfig
-from llama_index.embeddings.mistralai import MistralAIEmbedding
 from llama_index.core import Document
 
 config = LanguageConfig(language="english", spacy_model="en_core_web_md")
 
-class SemanticDoubleMergingSplitter:
+class SemanticDoubleMergingSplitter(SemanticDoubleMergingSplitterNodeParser):
     def __init__(self, 
                  language_config: LanguageConfig = config,
                  initial_threshold: float = 0.5,
                  appending_threshold: float = 0.7,
                  merging_threshold: float = 0.8,
                  max_chunk_size: int = 512):
-        self.splitter = SemanticDoubleMergingSplitterNodeParser(
+        """
+        Initializes the SemanticDoubleMergingSplitter.
+
+        :param language_config: LanguageConfig, default config. The language configuration for processing.
+        :param initial_threshold: float, default 0.5. The threshold for initial chunking.
+        :param appending_threshold: float, default 0.7. The threshold for appending chunks.
+        :param merging_threshold: float, default 0.8. The threshold for merging chunks.
+        :param max_chunk_size: int, default 512. The maximum size of a chunk.
+        """
+        super().__init__(
             language_config=language_config,
             initial_threshold=initial_threshold,
             appending_threshold=appending_threshold,
@@ -22,7 +27,13 @@ class SemanticDoubleMergingSplitter:
             max_chunk_size=max_chunk_size
         )
 
-    def process(self, _text: str):
-        document = Document(text=_text)
-        chunks = self.splitter.get_nodes_from_documents([document])
+    def process(self, text: str):
+        """
+        Splits the input text into chunks based on the defined thresholds.
+
+        :param text: str. The text to be split into chunks.
+        :return: list. A list of text chunks.
+        """
+        document = Document(text=text)
+        chunks = self.get_nodes_from_documents([document])
         return [chunk.text for chunk in chunks]
